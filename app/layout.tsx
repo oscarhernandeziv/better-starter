ee7a6d48b755e7131864d8d09e05ae3e4835a19d
oscarhernandeziv <oscarhernandeziv@gmail.com>
2025-03-17 19:29:03 -0700

    shadcn init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
@@ -18,14 +19,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link
 					rel="icon"
 					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
 				/>
 			</head>
-			<body className={`${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistMono.variable} antialiased`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { ThemeToggle } from "./_components/theme/theme-toggle";
+
 export default function Home() {
 	return (
 		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-mono)] sm:p-20">
 			<main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
 				<h1 className="font-bold text-4xl">Better Starter</h1>
 				<p className="text-lg">A Next.js 15 starter kit for 2025.</p>
+				<ThemeToggle />
 			</main>
 		</div>
 	);
